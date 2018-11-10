444b8346b43e51713685d7a2cdaf88958d4b45fa
Kai Mörker <kai.moerker@gmail.com>
2018-11-10 22:49:05 +0100

    use video module in PiTube

--- js/pitube.js
+++ js/pitube.js
@@ -1,6 +1,7 @@
 import * as DateHelper     from "./dateHelper.js";
 import * as DurationHelper from "./durationHelper.js";
 import * as HTTP           from "./http.js";
+import      Video          from "./video.js";
 
 const YOUTUBE_URL = "https://www.youtube.com/";
 
@@ -99,56 +100,6 @@ var Channel = function(username) {
             $videos.appendChild(video.render());
         });
     };
-}
-
-var Video = function(videoId) {
-    this.id = videoId;
-
-    this.load = function() {
-        let options = {
-            id: this.id,
-            maxResults: 1,
-            part: "snippet,contentDetails",
-            fields: "items(contentDetails%2Fduration%2Csnippet(publishedAt%2Cthumbnails%2Fmedium%2Furl%2Ctitle))"
-        }
-
-        return HTTP.request("/videos", options)
-            .then(res => res.json())
-            .then(data => {
-                data = data.items[0];
-
-                this.publishedAt  = new Date(data.snippet.publishedAt);
-                this.thumbnailUrl = data.snippet.thumbnails.medium.url;
-                this.duration     = data.contentDetails.duration;
-                this.title        = data.snippet.title;
-
-                return this;
-        });
-    }
-
-    this.render = function() {
-        var $video = document.createElement("div");
-        $video.classList.add("video");
-
-        $video.addEventListener("click", () => {
-            window.open(`${YOUTUBE_URL}watch?v=${this.id}`)
-        });
-
-        $video.innerHTML +=
-            `<img class="video-thumbnail" src="${this.thumbnailUrl}"/>` +
-            `<div class="video-title">${this.title}</div>` +
-            `<span class="video-info">` +
-                `<span class="icon-calendar fa fa-calendar"></span>` +
-                `${DateHelper.formatDate(this.publishedAt)}, ` +
-                DateHelper.formatTime(this.publishedAt) +
-            `</span>` +
-            `<span class="video-duration">` +
-                `<span class="icon-clock fa fa-clock-o"></span>` +
-                DurationHelper.format(this.duration) +
-            `</span>`;
-
-        return $video;
-    }
 }
 
 let channels = new ChannelCollection(USERNAMES);
