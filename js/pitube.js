588c5f9be01ee7ee883652f2cbb33e2d61000656
Kai Mörker <kai.moerker@gmail.com>
2018-11-10 23:28:24 +0100

    use channelCollection module in PiTube

--- js/pitube.js
+++ js/pitube.js
@@ -1,26 +1,9 @@
-import * as DateHelper     from "./dateHelper.js";
-import * as DurationHelper from "./durationHelper.js";
-import * as HTTP           from "./http.js";
-import      Video          from "./video.js";
-import      Channel        from "./channel.js";
-
-var ChannelCollection = function(usernames) {
-    this.channels = usernames.map(username => new Channel(username));
-
-    this.loadChannels = function() {
-        return Promise.all(this.channels.map(channel => channel.load()));
-    };
-
-    this.render = function() {
-        var $content = document.getElementById("content");
-
-        this.channels.forEach(channel => {
-            $content.appendChild(channel.render());
-
-            channel.renderVideos();
-        });
-    };
-}
+import * as DateHelper        from "./dateHelper.js";
+import * as DurationHelper    from "./durationHelper.js";
+import * as HTTP              from "./http.js";
+import      Video             from "./video.js";
+import      Channel           from "./channel.js";
+import      ChannelCollection from "./channelCollection.js";
 
 let channels = new ChannelCollection(USERNAMES);
 
