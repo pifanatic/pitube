08a0c0f0e7e101fb530c84e31a003c05ff7a9caf
Kai Mörker <kai.moerker@gmail.com>
2018-11-21 22:37:18 +0100

    PiTube - remove unused imports

--- js/pitube.js
+++ js/pitube.js
@@ -1,8 +1,3 @@
-import * as DateHelper        from "./helpers/dateHelper.js";
-import * as DurationHelper    from "./helpers/durationHelper.js";
-import * as HTTP              from "./lib/http.js";
-import      Video             from "./modules/video.js";
-import      Channel           from "./modules/channel.js";
 import      ChannelCollection from "./modules/channelCollection.js";
 
 let channels = new ChannelCollection(USERNAMES);
