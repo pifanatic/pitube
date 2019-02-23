af6d8f2fb4b5e45f93c9931f9e3647d2ba727e8a
Kai Mörker <kai.moerker@gmail.com>
2019-02-23 20:38:34 +0100

    DurationHelper - rewritten
    It works now!

--- js/helpers/durationHelper.js
+++ js/helpers/durationHelper.js
@@ -3,10 +3,25 @@ Takes a duration string as it has been returned by Youtube API and format
 it into better readable format: PT52M3S -> 52:03
 */
 export function format(durationString) {
-    return /PT(\d*)H?(\d*)M?(\d*)S?/
-            .exec(durationString)
-            .splice(1)
-            .filter(x => x) // remove undefined values
-            .map(x => x < 10 ? `0${x}` : x) // add leading 0 if necessary
-            .join(":");
+    let result  = "",
+        hours   = _getDurationPart(durationString, /(\d*)H/),
+        minutes = _getDurationPart(durationString, /(\d*)M/, "00"),
+        seconds = _getDurationPart(durationString, /(\d*)S/, "00");
+
+    if (hours) {
+        result += `${hours}:`;
+    }
+
+    result += `${minutes}:${seconds}`;
+
+    return result;
 };
+
+function _getDurationPart(durationString, regex, defaultValue) {
+    if (regex.test(durationString)) {
+        let time = regex.exec(durationString)[1];
+        return time < 10 ? `0${time}` : time;
+    } else {
+        return defaultValue || "";
+    }
+}
