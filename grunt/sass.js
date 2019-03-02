d896a4eceb4e502cc983ebf11d9f4266c41dc9d0
Kai Mörker <kai.moerker@gmail.com>
2019-03-02 15:09:51 +0100

    Grunt/sass - define production build options

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,14 +1,22 @@
 const sass = require("node-sass");
 
 module.exports = function(grunt) {
+    let files = {
+        "dist/css/default.css": "css/main.scss"
+    }
+
     grunt.config.merge({
         sass: {
             options: {
                 implementation: sass
             },
             devel: {
-                files: {
-                    "dist/css/default.css": "css/main.scss"
+                files: files
+            },
+            prod: {
+                files: files,
+                options: {
+                    outputStyle: "compressed"
                 }
             }
         }
