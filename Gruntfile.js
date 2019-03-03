3d8fc9808ddd56062faae3d10a257550e364c96a
Kai Mörker <kai.moerker@gmail.com>
2019-03-03 19:12:58 +0100

    Grunt - define defaultTasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,16 @@
 module.exports = function(grunt) {
 
+    let defaultTasks = ["clean", "copy"];
+
     if (!grunt.file.exists("config.js")) {
         grunt.fail.fatal(`No configuration file found!`);
     }
 
     grunt.loadTasks("./grunt/");
 
-    grunt.registerTask("devel", ["clean", "copy", "sass:devel"]);
+    grunt.registerTask("devel", defaultTasks.concat(["sass:devel"]));
 
-    grunt.registerTask("prod", ["clean", "copy", "sass:prod"]);
+    grunt.registerTask("prod", defaultTasks.concat(["sass:prod"]));
 
     grunt.registerTask("watch", ["chokidar"]);
 }
