ef8ebc278f463b6364d532a035a35d8a916e907d
Kai Mörker <kai.moerker@gmail.com>
2019-03-16 11:16:09 +0100

    Lang - allow placeholders in keys

--- js/lib/lang.js
+++ js/lib/lang.js
@@ -9,17 +9,26 @@ class Lang {
      * Returns the translation for the current language for a lang key
      *
      * @param {String} key The lang key
+     * @param {String} value A value to replace placeholders with
      *
      * @return Locale of the key in current language or a string indicating that
      *         key or translation for this key does not exist in current language
      */
-    get(key) {
+    get(key, value) {
+        let locale;
+
         if (!Keys[key]) {
             return `Unknown key ${key}`;
         }
 
-        return Keys[key][this.currentLang] ||
-               `Missing translation for ${key} in language ${this.currentLang}!`;
+        locale = Keys[key][this.currentLang] ||
+                 `Missing translation for ${key} in language ${this.currentLang}!`;
+
+        if (value) {
+            locale = locale.replace("<X>", value);
+        }
+
+        return locale;
     }
 
     /**
