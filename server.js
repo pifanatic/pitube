e915558c45d833b707c6b98bc0bb14e678e80580
Kai Mörker <kai.moerker@gmail.com>
2020-06-14 01:32:06 +0200

    remove some wild indentations

--- server.js
+++ server.js
@@ -31,23 +31,19 @@ server.use(express.static("dist"));
 server.get("/api/channels", (req, res) => {
     updateQuota(3);
 
-    res.json(
-        {
-            "items": [
-                {
-                    "id": req.query.id,
-                    "snippet": {
-                        "title": `channel_name`,
-                        "thumbnails": {
-                            "default": {
-                                "url": `develAssets/channel_avatar.jpg`
-                            }
-                        }
+    res.json({
+        "items": [{
+            "id": req.query.id,
+            "snippet": {
+                "title": `channel_name`,
+                "thumbnails": {
+                    "default": {
+                        "url": `develAssets/channel_avatar.jpg`
                     }
                 }
-            ]
-        }
-    );
+            }
+        }]
+    });
 });
 
 server.get("/api/search", (req, res) => {
@@ -69,26 +65,22 @@ server.get("/api/search", (req, res) => {
 server.get("/api/videos", (req, res) => {
     updateQuota(4);
 
-    res.json(
-        {
-            "items": [
-                {
-                    "snippet": {
-                        "publishedAt": "2020-02-05T15:24:19Z",
-                        "title": "test_video_title",
-                        "thumbnails": {
-                            "medium": {
-                                "url": "develAssets/video.jpg"
-                            }
-                        }
-                    },
-                    "contentDetails": {
-                        "duration": "PT12M45S"
+    res.json({
+        "items": [{
+            "snippet": {
+                "publishedAt": "2020-02-05T15:24:19Z",
+                "title": "test_video_title",
+                "thumbnails": {
+                    "medium": {
+                        "url": "develAssets/video.jpg"
                     }
                 }
-            ]
-        }
-    );
+            },
+            "contentDetails": {
+                "duration": "PT12M45S"
+            }
+        }]
+    });
 });
 
 server.listen(8080);
